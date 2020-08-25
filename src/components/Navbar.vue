ed980c1f40ce8b3be09d2eaa444b5a186ce69a73
Matias <matias.lazos@gmail.com>
2020-08-25 01:25:27 -0400

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="nav">
+    <div class="logo">
+      <img src="./../assets/logo.svg" alt="logo" />
+    </div>
+    <router-link to="/">Features</router-link>
+    <router-link to="/about">Team</router-link>
+    <router-link to="/about">Sign in</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#nav {
+   margin: 0 auto;
+  display: flex;
+width: 1240px;
+  font-family: "Raleway", sans-serif;
+  .logo{
+      display:flex;
+      flex-grow: 1;
+  }
+  padding: 60px;
+  a {
+      
+    display: flex;
+    // flex-grow: 1;
+    padding-left: 60px;
+    font-weight: bold;
+    color: white;
+    font-size: 16px;
+
+    // &.router-link-exact-active {
+    //   color: #42b983;
+    // }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="intro-image">
+      <img src="./../assets/illustration-intro.png" alt="">
+    </div>
+    <h1>All your files in one secure location, <br/> accessible anywhere.</h1>
+    <h2>Fylo stores all your most important files in one secure<br/>location. Access them wherever you need, share and<br/>collaborate with friends family, and co-workers.</h2>
+    <div class="fylo-button">
+      <p><b>Get Started</b></p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style lang="scss" scoped>
+h1{
+  padding-top: 60px;
+  font-size: 40px;
+  font-family: "Raleway", sans-serif;
+}
+h2{
+  padding-top: 30px;
+  font-size: 20px;
+}
+
+.fylo-button{
+display:flex;
+align-items: center;
+justify-content: center;
+margin: 0 auto;
+margin-top: 40px;
+  width: 280px;
+  min-height: 56px;
+  background-color: #41B2D0;
+  border-radius: 36px;
+  color: white;
+  p{
+    padding: 0px;
+  }
+}
+</style>
